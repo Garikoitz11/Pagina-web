49c08dab33e4ca8f33dd821ee07dfac4f8b78f84
Janireee <janirevgg@gmail.com>
2021-10-18 20:43:53 +0200

    Cambios nuevos

--- Gartxon/Webgunea/public_html/js/formulario.js
+++ Gartxon/Webgunea/public_html/js/formulario.js
@@ -1,9 +1,9 @@
-function validarPosta(posta) {
+function onartuPosta(posta) {
     var posta = /^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/;
     return posta.test(posta);
     }
 
-    function validarMugikorra(tel){
+    function onartuMugikorra(tel){
         var tel= /^\d{9}$/;
         if(!tel.test(tel)){
             return false;
