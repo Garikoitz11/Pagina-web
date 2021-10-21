80b3d2032a1175300551afebf05eee3f59c63a95
Unai Solaun <unai.solaun@zaraobe.net>
2021-10-21 21:30:22 +0200

    correccion

--- Gartxon/Webgunea/public_html/php/produktuakKontsultatu.php
+++ Gartxon/Webgunea/public_html/php/produktuakKontsultatu.php
@@ -9,8 +9,8 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <meta name="viewport" content"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <link rel ="stylesheet" href="CSS/produktuak.css">
+        <meta name="viewport" content = "width=device-width", user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0>
+        <link rel ="stylesheet" type="text/css" href="CSS/Estilo proba.css">
     </head>
     <body>
         <div class="container-table">
