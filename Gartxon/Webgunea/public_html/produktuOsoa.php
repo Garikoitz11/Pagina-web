8f44c05ef2ff9430c9052ff641812070b69a8b1c
Janireee <janirevgg@gmail.com>
2021-10-23 15:08:54 +0200

    tablas

--- Gartxon/Webgunea/public_html/produktuOsoa.php
+++ Gartxon/Webgunea/public_html/produktuOsoa.php
@@ -12,7 +12,7 @@
         <title>Gartxon S.L</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width", user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0>
-        <link rel="stylesheet" type="text/css" href="./CSS/produktuak.css">
+        <link rel="stylesheet" type="text/css" href="./CSS/produktuOsoa.css">
     </head>
     <body>
         <div class="container-table">
@@ -22,6 +22,8 @@
             <div class="table__header">Mota</div>
             <div class="table__header">Deskribapena</div>
             <div class="table__header">Prezioa</div>
+            <div class="table__header"></div>
+            <div class="table__header"></div>
             <?php $emaitza = mysqli_query($conexion, $erabiltzaile);
             while($row=mysqli_fetch_assoc($emaitza)){?>
             <div class="table__item"><?php echo $row["Kodea"];?></div>
